073e0b45429aebded9ccf96e98f14d8c6ea2ea9c
Tommy Lau <tlau425@yahoo.com>
2019-02-28 03:17:27 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,7 +1,9 @@
 nums = [1,2,3,4,5]
+
 def my_select(collection)
  # your code here!
  new_array = []
- new_array += collection.select { |num| num.even? }
-end
+ collection.select do |x|
+   x.even?
+  end
 end
